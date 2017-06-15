aa5e9bb22ffafa679932ecdc2b21a98c7d0b359a
Markus Manninen <markus.manninen@futurice.com>
2017-06-15 12:55:53 +0300

    Add queries playground file

---
+++ playground/mongoose-queries.js
@@ -0,0 +1,37 @@
+const { ObjectID } = require('mongodb')
+
+const { mongoose } = require('./../server/db/mongoose')
+const { Todo } = require('./../server/models/todo')
+const { User } = require('./../server/models/user')
+
+// const id = '5942545036609c080bca4cb411'
+//
+// if (!ObjectID.isValid(id)) {
+//   console.log('ID not valid')
+// }
+
+// Todo.find({
+//   _id: id
+// }).then((todos) => {
+//   console.log('Todos', todos)
+// })
+//
+// Todo.findOne({
+//   _id: id
+// }).then((todo) => {
+//   console.log('Todo', todo)
+// })
+//
+// Todo.findById(id).then((todo) => {
+//   if (!todo) {
+//     return console.log('Id not found')
+//   }
+//   console.log('Todo by Id', todo)
+// }).catch((e) => console.log(e))
+
+User.findById('593572ef46510f9615b72ccf').then((user) => {
+  if (!user) {
+    return console.log('User not found')
+  }
+  console.log(JSON.stringify(user, undefined, 2))
+}, (e) => console.log(e))
