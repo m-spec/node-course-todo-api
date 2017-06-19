1f41f59fafb80c61b88dea065c7a8a4fb436b97c
Markus Manninen <markus.manninen@futurice.com>
2017-06-19 16:26:43 +0300

    Hash user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,5 +1,20 @@
 const { SHA256 } = require('crypto-js')
 const jwt = require('jsonwebtoken')
+const bcrypt = require('bcryptjs')
+
+const password = '123abc!'
+
+// bcrypt.genSalt(10, (err, salt) => {
+//   bcrypt.hash(password, salt, (err, hash) => {
+//     console.log(hash);
+//   })
+// })
+
+const hashedPassword = '$2a$10$Q3uPR2btQfypAHFqvkR3y.AU1Ab6u6k.4uSkYipfZeKM89NIFnIuu'
+
+bcrypt.compare(password, hashedPassword, (err, res) => {
+  console.log(res);
+})
 
 const data = {
   id: 10
