3e6d2d6a89b9059a974f57872e1a833ae702af25
Markus Manninen <markus.manninen@futurice.com>
2017-06-19 16:04:02 +0300

    Add GET /users/me and authentication middleware

--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ const _ = require('lodash')
 const { mongoose } = require('./db/mongoose')
 const { Todo } = require('./models/todo')
 const { User } = require('./models/user')
+const { authenticate } = require('./middleware/authenticate')
 
 const app = express()
 
@@ -109,6 +110,10 @@ app.post('/users', (req, res) => {
   }).catch((err) => {
     res.status(400).send(err)
   })
+})
+
+app.get('/users/me', authenticate, (req, res) => {
+  res.send(req.user)
 })
 
 app.listen(port, () => {
